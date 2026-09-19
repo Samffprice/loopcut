5696637bcd75f03e52a6b92ea447101ee13328cd
Samuel Price <samffprice@tamu.edu>
2026-09-19 17:37:24 -0500

    Loopcut 0.1.1

--- scripts/addons_core/loopcut/__init__.py
+++ scripts/addons_core/loopcut/__init__.py
@@ -8,7 +8,7 @@ bl_info = {
     "name": "Loopcut",
     "description": "An AI agent that works inside Blender",
     "author": "Loopcut",
-    "version": (0, 1, 0),
+    "version": (0, 1, 1),
     "blender": (5, 2, 0),
     "location": "Editor Type > Loopcut",
     "category": "Interface",
